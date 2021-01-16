f5f1458dee43f784154722044ee9449252412b47
Sachitha Hirushan <shpsachitha@gmail.com>
2021-01-16 12:11:17 +0530

    customer model created

---
+++ app/src/main/java/com/example/sqlitedemo/CustomerModel.java
@@ -0,0 +1,72 @@
+package com.example.sqlitedemo;
+
+public class CustomerModel {
+
+    private int id;
+    private String name;
+    private int age;
+    private boolean isActive;
+
+
+
+
+    //to string
+
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+
+    //constructor
+
+
+    public CustomerModel(int id, String name, int age, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public CustomerModel() {
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
